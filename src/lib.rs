3cd6cabcb2605fd67337e250671f43cd75251eb6
Paul Delafosse <paul.delafosse@protonmail.com>
2021-12-03 17:37:12 +0100

    test: add env vars tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod prelude;
 
 #[cfg(test)]
 mod tests {
+    use std::env::VarError;
     use cmd_lib::run_cmd;
     use crate::prelude::*;
 
@@ -23,12 +24,32 @@ mod tests {
     }
 
     #[temp_test]
+    fn a_dummy_test_with_var() -> Result<(), VarError> {
+        std::env::set_var("VAR", "bar");
+
+        let var = std::env::var("VAR")?;
+
+        assert_eq!(var, "bar");
+        Ok(())
+    }
+
+    #[temp_test]
+    fn a_dummy_test_with_another_var() -> Result<(), VarError> {
+        std::env::set_var("VAR", "foo");
+
+        let var = std::env::var("VAR")?;
+
+        assert_eq!(var, "foo");
+        Ok(())
+    }
+
+    #[temp_test]
     fn another_dummy_test_with_git() {
         let current_dir = std::env::current_dir().unwrap();
         run_cmd! (
             info "Initializing test repo in $current_dir";
             git init;
-            git commit -m a commit --allow-empty;
+            git commit -m "a commit" --allow-empty;
             git checkout -b branch1;
             git shortlog;
         ).unwrap();
